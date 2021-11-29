75254e34095759319b9223cb6a76a8b18ceddf7e
yujeong lim <anneu5712@gmail.com>
2021-11-29 17:00:29 +0900

    part7 - customize admin

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,21 @@ from django.contrib import admin
 from .models import Question, Choice
 
 # Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)  # 관리자페이지에 Choice 추가
+
+class ChoiceInline(admin.TabularInline): # TabularInline을 사용해 테이블 기반 형식으로 표시
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+    fieldsets = [  # 필드를 묶어서 제목을 준다
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]  # question페이지 안에서 choice를 관리할 수 있도록 한다.
+
+    list_filter = ['pub_date']  # 발행일을 기준으로 필터링 할 수 있는 기능 제공
+    search_fields = ['question_text']  # 검색창 기능 제공
+
+admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 
 # Create your models here.
@@ -11,7 +12,12 @@ class Question(models.Model):
     
     def __str__(self):
         return self.question_text
-
+    
+    @admin.display(
+        boolean=True,  # True, False를 아이콘으로 변경
+        ordering='pub_date',
+        description='Published recently?',
+    )
     def was_published_recently(self):
         # 버그 수정
         now = timezone.now()
